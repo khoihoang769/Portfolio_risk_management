f72103a48b1bffbb88b41234972df11186390282
khoihoang769 <48714854+khoihoang769@users.noreply.github.com>
2020-07-23 09:21:19 +0700

    Add files via upload

--- EquitySimPortfolio.py
+++ EquitySimPortfolio.py
@@ -18,7 +18,6 @@ abbv = FinancialInfo('ABBV')
 pfe = FinancialInfo('PFE')
 azn = FinancialInfo('AZN')
 microsoft = FinancialInfo('MSFT')
-jmia = FinancialInfo('JMIA')
 tesla = FinancialInfo('TSLA')
 
 amazon.get_data()
@@ -26,17 +25,16 @@ abbv.get_data()
 pfe.get_data()
 azn.get_data()
 microsoft.get_data()
-jmia.get_data()
 tesla.get_data()
-# FinancialInfo.port_rets([0.4416, 0.1559, 0.1417, 0.0855, 0.0587,0.04])
+FinancialInfo.port_rets([0.4416, 0.1559, 0.1417, 0.0855, 0.0587,0.04])
 
-# portfolio_volatility = FinancialInfo.portfolio_volatility
+portfolio_volatility = FinancialInfo.portfolio_volatility
 
-# print (portfolio_volatility)
+print (portfolio_volatility)
 
-# FinancialInfo.optimization(900000)
-# FinancialInfo.monte_carlo(2500)
-# FinancialInfo.get_efficient_frontier(FinancialInfo.portfolio_weights)
+FinancialInfo.optimization(900000)
+FinancialInfo.monte_carlo(2500)
+FinancialInfo.get_efficient_frontier(FinancialInfo.portfolio_weights)
 
 S = tesla.tickerdf['Close'][-1]
 
@@ -47,6 +45,6 @@ amzn3000p = amazon.put_value(3000,0.25)
 print (round(amzn3800c,3))
 print (round(amzn3000p,3))
 print (round(tesla.call_value(2000,0.25),3))
-# abbv.rolling_ma(20)
-# abbv.get_stock_sharpe()
-# print (tesla.tickerdf.tail(5))
+
+abbv.rolling_ma(20)
+abbv.get_stock_sharpe()
